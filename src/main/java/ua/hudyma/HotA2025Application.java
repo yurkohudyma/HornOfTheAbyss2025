d5b6301f0f3a8c285d8d9f37a6d1752ca4fa7bdd
yurkohudyma <hudyma@gmail.com>
2025-12-04 06:30:22 +0200

    init commit

---
+++ src/main/java/ua/hudyma/HotA2025Application.java
@@ -0,0 +1,11 @@
+package ua.hudyma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotA2025Application {
+    public static void main(String[] args) {
+        SpringApplication.run(HotA2025Application.class, args);
+    }
+}
