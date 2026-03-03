87af86e1dfd2e26d4a6d033236ab3fd597a7306b
yurkohudyma <hudyma@gmail.com>
2026-03-03 20:48:37 +0200

    renamed mines like in HOTA

--- src/main/java/ua/hudyma/resource/enums/MineType.java
+++ src/main/java/ua/hudyma/resource/enums/MineType.java
@@ -10,11 +10,11 @@ import static ua.hudyma.resource.enums.ResourceType.*;
 public enum MineType {
     GOLDEN_MINE (GOLD, 1000),
     ALCHEMIST_LAB (MERCURY, 1),
-    GEM_MINE (GEMS, 1),
+    GEM_POND (GEMS, 1),
     SAWMILL (WOOD, 2),
-    QUARRY (ORE, 2),
-    CRYSTAL_MINE (CRYSTAL, 1),
-    SULFUR_MINE (SULFUR, 1);
+    ORE_PIT (ORE, 2),
+    CRYSTAL_CAVERN (CRYSTAL, 1),
+    SULFUR_DUNE (SULFUR, 1);
     private final ResourceType resourceType;
     private final Integer weeklyProductionRate;
 }
