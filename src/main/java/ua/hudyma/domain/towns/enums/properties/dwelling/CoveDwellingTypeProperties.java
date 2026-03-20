a15e049b12795dc503bd5992c83015cb26084c2b
yurko_work <hudyma@gmail.com>
2026-03-20 16:44:49 +0200

    CoveDwellingTypeProperties update

--- src/main/java/ua/hudyma/domain/towns/enums/properties/dwelling/CoveDwellingTypeProperties.java
+++ src/main/java/ua/hudyma/domain/towns/enums/properties/dwelling/CoveDwellingTypeProperties.java
@@ -4,74 +4,76 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import ua.hudyma.domain.towns.enums.CommonBuildingType;
 import ua.hudyma.domain.towns.enums.FortificationType;
-import ua.hudyma.domain.towns.enums.UniqueBuildingType;
 import ua.hudyma.domain.towns.enums.dwelling.AbstractDwellingTypeProperties;
-import ua.hudyma.domain.towns.enums.dwelling.CastleDwellingType;
+import ua.hudyma.domain.towns.enums.dwelling.CoveDwellingType;
 import ua.hudyma.resource.enums.ResourceType;
 
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 
+import static ua.hudyma.domain.towns.enums.CommonBuildingType.BLACKSMITH;
 import static ua.hudyma.resource.enums.ResourceType.*;
 
 @Getter
 @RequiredArgsConstructor
 public enum CoveDwellingTypeProperties implements AbstractDwellingTypeProperties {
-    //todo correct values to match HOTA
     WATERFALL(toStringMap(
             Map.of(FortificationType.FORT.name(), 0)),
             toResourceEnumMap(
                     Map.of(ORE, 5,
-                            GOLD, 500))),
+                            WOOD, 5,
+                            GOLD, 300))),
     SHACK(toStringMap(
-            Map.of(CastleDwellingType.GUARDHOUSE.name(), 0)),
+            Map.of(CoveDwellingType.WATERFALL.name(), 0)),
             toResourceEnumMap(
                     Map.of(
                             WOOD, 5,
-                            ORE, 5,
-                            GOLD, 500))),
-    WAREHOUSE(toStringMap(
-            Map.of(CastleDwellingType.BARRACKS.name(), 0)),
-            toResourceEnumMap(
-                    Map.of(
-                            ORE, 5,
                             GOLD, 1000))),
-    NEST(toStringMap(Map.of(
-            CommonBuildingType.BLACKSMITH.name(), 0,
-            CastleDwellingType.GUARDHOUSE.name(), 0)),
+    WAREHOUSE(toStringMap(
+            Map.of(SHACK.name(), 0,
+                    BLACKSMITH.name(), 0)),
             toResourceEnumMap(
                     Map.of(
+                            ORE, 5,
+                            WOOD,5,
+                            CRYSTAL, 5,
+                            GEMS,5,
+                            SULFUR,5,
+                            MERCURY,5,
+                            GOLD, 3000))),
+    NEST(toStringMap(Map.of(
+            SHACK.name(), 0)),
+            toResourceEnumMap(
+                    Map.of(CRYSTAL, 2,
+                            GEMS,2,
                             ORE, 5,
                             GOLD, 2000))),
     TOWER_OF_THE_SEAS(toStringMap(Map.of(
             CommonBuildingType.MAGE_GUILD.name(), 1,
-            CastleDwellingType.BARRACKS.name(), 0)),
+            SHACK.name(), 0)),
+            toResourceEnumMap(
+                    Map.of(
+                            CRYSTAL, 5,
+                            SULFUR, 5,
+                            GOLD, 2000))),
+    NIX_FORT(toStringMap(Map.of(
+            NEST.name(), 0)),
             toResourceEnumMap(
                     Map.of(
                             WOOD, 5,
-                            ORE, 5,
-                            MERCURY, 2,
-                            CRYSTAL, 2,
-                            SULFUR, 2,
-                            GEMS, 2,
+                            ORE, 10,
+                            SULFUR, 5,
                             GOLD, 3000))),
-    NIX_PORT(toStringMap(Map.of(
-            UniqueBuildingType.STABLES.name(), 0,
-            CastleDwellingType.BARRACKS.name(), 0)),
-            toResourceEnumMap(
-                    Map.of(
-                            WOOD, 20,
-                            GOLD, 5000))),
     MAELSTROM(toStringMap(
-            Map.of(CastleDwellingType.MONASTERY.getCode(), 0)),
+            Map.of(TOWER_OF_THE_SEAS.name(), 0,
+                    NIX_FORT.name(), 0)),
             toResourceEnumMap(
                     Map.of(
-                            MERCURY, 10,
-                            CRYSTAL, 10,
-                            SULFUR, 10,
-                            GEMS, 10,
-                            GOLD, 20000)));
+                            WOOD, 15,
+                            CRYSTAL, 15,
+                            SULFUR, 20,
+                            GOLD, 15000)));
     private final Map<String, Integer> requiredBuildingMap;
     private final EnumMap<ResourceType, Integer> requiredResourceMap;
     private static EnumMap<ResourceType, Integer> toResourceEnumMap(
@@ -80,11 +82,8 @@ public enum CoveDwellingTypeProperties implements AbstractDwellingTypeProperties
         map.putAll((resources));
         return map;
     }
-    private static <T extends Enum<T>> Map<? extends Enum<T>, Integer> toMap(
-            Map<? extends Enum<T>, Integer> source) {
-        return new HashMap<>(source);
-    }
-    private static Map<String, Integer> toStringMap (Map<String, Integer> source){
+    private static Map<String, Integer> toStringMap (
+            Map<String, Integer> source){
         return new HashMap<>(source);
     }
 }
